a4e15885ff35e5f887960e788250c591aa84b937
Keeanu Rodriguez <keeanu7@gmail.com>
2023-01-20 00:05:17 -0500

    all test passed

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const base_url = "https://resource-ghibli-api.onrender.com/films";
+const base_url = "https://resource-ghibli-api.onrender.com";
 const select = document.querySelector("#titles");
 const h3 = document.createElement("h3");
 const div = document.querySelector("div");
@@ -8,42 +8,52 @@ const reviewForm = document.querySelector(".reviewForm");
 const reviewText = document.getElementById("review");
 const ul = document.querySelector("ul");
 const resetReviews = document.querySelector("#reset-reviews");
+const peopleButton = document.querySelector("button");
+const ol = document.querySelector(".here")
 
 // To ensure Cypress tests work as expeded, add any code/functions that you would like to run on page load inside this function
 
 function run() {
-    fetch(base_url)
+    fetch(`${base_url}/films`)
     .then((response) => response.json())
         .then((filmResult) => {
             grabTitles(filmResult);
             getDescription(filmResult);
         })
-    .catch((error) => console.log(error))
+        .catch((error) => console.err(error));
 }
 
 function grabTitles(filmResult) {
     for (let i = 0; i < filmResult.length; i++) {
         const option = document.createElement("option");
         option.value = filmResult[i].id;
-        option.innerHTML = filmResult[i].title;
+        option.innerText = filmResult[i].title;
         select.append(option);
     }
 }
 
 
 function getDescription(filmResult) {
-    select.addEventListener("change", () => {
+    select.addEventListener("change", (event) => {
+        event.preventDefault();
+        fetch(`${base_url}/people`)
+                .then((response) => response.json())
+                .then((peopleResult) => {
+                    showPeople(peopleResult);
+                })
+                .catch((error) => console.log(error));
+        ol.innerHTML = ""
         for (let i = 0; i < filmResult.length; i++) {
             if (select.value === filmResult[i].id) {
                 div.append(h3);
                 div.append(pYear);
                 div.append(pDescription);
 
-                h3.innerHTML = filmResult[i].title;
-                pYear.innerHTML = filmResult[i].release_date;
-                pDescription.innerHTML = filmResult[i].description;
-            }
-            
+                h3.setAttribute("value", filmResult[i].id)
+                h3.innerText = filmResult[i].title;
+                pYear.innerText = filmResult[i].release_date;
+                pDescription.innerText = filmResult[i].description;
+            }           
         }
     })
 }
@@ -58,8 +68,8 @@ reviewForm.addEventListener("submit", (event) => {
 
     const li = document.createElement("li");
     const strong =document.createElement("strong");
-        strong.innerHTML = `${h3.innerHTML}: `;
-    li.innerHTML = userInput;
+    strong.innerText = `${h3.innerHTML}: `;
+    li.innerText = userInput;
     li.prepend(strong);
     ul.append(li);
 
@@ -69,7 +79,36 @@ reviewForm.addEventListener("submit", (event) => {
     }
 });
 
+select.addEventListener("change", () => {
+    fetch(`${base_url}/people`)
+        .then((response) => response.json())
+        .then((peopleResult) => {
+            showPeople(peopleResult);
+        })
+        .catch((error) => console.log(error));
+})
 
+peopleButton.addEventListener("click", () => {
+    ol.style.display = "block";
+    fetch(`${base_url}/people`)
+        .then((response) => response.json())
+        .then((peopleResult) => {
+            showPeople(peopleResult);
+        })
+        .catch((error) => console.log(error));
+})
+
+function showPeople(peopleResult) {
+    console.log(h3);
+    for (let i = 0; i < peopleResult.length; i++) {
+        if (h3.getAttribute("value") === peopleResult[i].films[0].slice(7)) {
+            const li = document.createElement("li");
+            li.style.display = 'none'
+            li.innerText = peopleResult[i].name;
+            ol.append(li);
+        }
+    }
+}
 
 
 
